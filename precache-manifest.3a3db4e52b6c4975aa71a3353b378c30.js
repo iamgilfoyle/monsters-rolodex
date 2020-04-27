acee74615b35ae63e997970acc03d806662ef0e3
imagilfoyle <abreux@gmail.com>
2020-04-27 18:48:18 +0100

    Updates

---
+++ precache-manifest.3a3db4e52b6c4975aa71a3353b378c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7396f8841c2120da208b729d70581a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "99af6d9203a85c0f7e10",
+    "url": "/monsters-rolodex/static/css/main.6d2f186d.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99af6d9203a85c0f7e10",
+    "url": "/monsters-rolodex/static/js/main.d526344b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
